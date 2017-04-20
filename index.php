df3f90a39b7984c5e9a35f852abfa40e4079343d
David J Eddy <me@davidjeddy.com>
2017-04-19 22:39:06 -0400

    I see that sneaky last line, using a  instead of the expected , its ok, I got you!

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
-declare(strict_type=1);
+declare(strict_types=1);
 
-echo 'Array soring practice'.
+echo "Array soring practice.\n";
+echo "\n";
 
+$array = [];
+
+// @source http://stackoverflow.com/questions/13246597/how-to-read-a-file-line-by-line-in-php
+$handle = fopen("source_data.txt", "r");
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+    	$lineData = explode(' - ', $line);
+    	$array[$lineData[0]] = $lineData[1];
+        // process the line read.
+    }
+
+    fclose($handle);
+} else {
+    // error opening the file.
+} 
+
+print_r($array);
+exit(0);
