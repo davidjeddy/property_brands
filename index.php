e48341c78e247d547527f008b55508852e5da525
David J Eddy <me@davidjeddy.com>
2017-04-21 18:32:58 -0400

    Iteration backed on peer feedback.

--- index.php
+++ index.php
@@ -7,98 +7,104 @@ declare(strict_types=1);
 class processDataSet
 {
     /**
-     * [$filename description]
      * @var string
      */
     private $filename = './source_data.txt';
 
     /**
-     * [$seperators description]
-     * @var [type]
+     * @var array
      */
     public $seperators = [' - ', ' – '];
 
     /**
-     * @param  [type] $paramData [description]
-     * @return [type]            [description]
+     * @var array
+     */
+    public $data = [];
+
+    /**
+     * processDataSet constructor.
+     *
+     * @param null $paramData
      */
     public function __construct($paramData = null)
      {
 
-        $this->exec( ($paramData ?: $this->filename) );
+        $this->run( ($paramData ?: $this->filename) );
     }
 
     /**
-     * @param  string $paramData [description]
-     * @return [type]            [description]
+     * @param string $paramData
+     *
+     * @return bool
      */
-    private function exec(string $paramData)
+    private function run(string $paramData)
     {
-        $returnData = [];
-
         $handle = fopen($paramData, "r");
-        if ($handle !== null) {
 
-            // read each line of the data source text file
-            while (($line = fgets($handle)) !== false) {
+        if ($handle == false) {
+            return false;
+        }
 
-                foreach ($this->seperators as $sepOption) {
-                    // does the string have an allowable seperator string?
-                    if (strpos($line, $sepOption) > 0) {
-                        // explode string and push onto return array
-                        $tmp = explode($sepOption, $line);
-                        $returnData[$tmp[0]] = $tmp[1];
-                    }
+        // read each line of the data source text file
+        while (($line = fgets($handle)) !== false) {
+
+            foreach ($this->seperators as $sepOption) {
+                // does the string have an allowable separator string?
+                if (strpos($line, $sepOption) > 0) {
+                    // explode string and push onto return array
+                    $tmp = explode($sepOption, $line);
+                    $this->data[$tmp[0]] = $this->removeLineBreak($tmp[1]);
                 }
             }
+        }
 
-            // close the file resource
-            fclose($handle);
-        } else {
-            // error opening the file.
-        } 
+        // close the file resource
+        fclose($handle);
 
         // sort lines
-        $returnData = $this->sortArray($returnData);
+         $this->sortArray();
 
         // output now sorted data
-        $this->output($returnData);
+        $this->output();
+
+        return true;
     }
 
     /**
-     * @param  array  $paramData [description]
-     * @return [type]            [description]
+     * @return bool
      */
-    private function sortArray(array $paramData) : array
+    private function sortArray()
     {
-        array_multisort(array_keys($paramData), SORT_NATURAL, $paramData);
+        return array_multisort(array_keys($this->data), SORT_NATURAL, $this->data);
 
-        return $paramData;
     }
 
     /**
-     * @param  array  $paramData [description]
-     * @return [type]            [description]
+     *
      */
-    private function output(array $paramData)
+    private function output()
     {
         $output = implode("\n", array_map(
             function ($v, $k) { return sprintf("%s - %s", $k, $v); },
-            $paramData,
-            array_keys($paramData)
+            $this->data,
+            array_keys($this->data)
         ));
 
-        fwrite(STDOUT, $this->removeLineBreak($output));
+        fwrite(STDOUT, $output);
+
+        exit(0);
     }
 
     /**
      * @source http://stackoverflow.com/questions/709669/how-do-i-remove-blank-lines-from-text-in-php
-     * @param  string $paramData [description]
-     * @return [type]            [description]
+     *
+     * @param string $param
+     *
+     * @return string
      */
-    private function removeLineBreak(string $paramData) : string
+    private function removeLineBreak(string $param) : string
     {
-        return preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $paramData);
+        return preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", null, $param);
     }
 }
 
