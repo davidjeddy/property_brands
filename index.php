531d2ea336388518c9ca3b3bb5f0b43bfebb9906
David J Eddy <me@davidjeddy.com>
2017-04-23 23:04:45 -0400

    After finding the data set in the request email is incorrect, compared to the data set in the attachment, iterated on logic to be based on attachment data set.

--- index.php
+++ index.php
@@ -14,32 +14,14 @@ class processDataSet
     /**
      * @var array
      */
-    public $seperators = [' - ', ' – '];
-
-    /**
-     * @var array
-     */
     public $data = [];
 
     /**
-     * processDataSet constructor.
-     *
-     * @param null $paramData
+     * 
      */
-    public function __construct($paramData = null)
-     {
-
-        $this->run( ($paramData ?: $this->filename) );
-    }
-
-    /**
-     * @param string $paramData
-     *
-     * @return bool
-     */
-    private function run(string $paramData)
+    public function __construct()
     {
-        $handle = fopen($paramData, "r");
+        $handle = fopen($this->filename, "r");
 
         if ($handle == false) {
             return false;
@@ -47,23 +29,14 @@ class processDataSet
 
         // read each line of the data source text file
         while (($line = fgets($handle)) !== false) {
-
-            foreach ($this->seperators as $sepOption) {
-                // does the string have an allowable separator string?
-                if (strpos($line, $sepOption) > 0) {
-                    // explode string and push onto return array
-                    $tmp = explode($sepOption, $line);
-                    var_dump($tmp[1]);
-                    $this->data[$tmp[0]] = $this->removeLineBreak($tmp[1]);
-                }
-            }
+            $this->data[] = $line;
         }
 
         // close the file resource
         fclose($handle);
 
         // sort lines
-         $this->sortArray();
+        $this->sortArray();
 
         // output now sorted data
         $this->output();
@@ -76,8 +49,7 @@ class processDataSet
      */
     private function sortArray()
     {
-        return array_multisort(array_keys($this->data), SORT_NATURAL, $this->data);
-
+        return natsort($this->data);
     }
 
     /**
@@ -85,27 +57,9 @@ class processDataSet
      */
     private function output()
     {
-        $output = implode("\n", array_map(
-            function ($v, $k) { return sprintf("%s - %s", $k, $v); },
-            $this->data,
-            array_keys($this->data)
-        ));
-
-        fwrite(STDOUT, $output);
+        fwrite(STDOUT, implode("\n", $this->data));
 
         exit(0);
-    }
-
-    /**
-     * @source http://stackoverflow.com/questions/709669/how-do-i-remove-blank-lines-from-text-in-php
-     *
-     * @param string $param
-     *
-     * @return string
-     */
-    private function removeLineBreak(string $param) : string
-    {
-        return preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", null, $param);
     }
 }
 
